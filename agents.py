93a10d04c1ebd831cdb87bc8df4932135613f355
yusuf emad <yusufy2004@gmail.com>
2023-12-03 23:54:04 +0200

    fds

--- agents.py
+++ agents.py
@@ -8,32 +8,32 @@ from retrievers import *
 from tools import get_tool
 from langchain.agents.openai_functions_agent.agent_token_buffer_memory import AgentTokenBufferMemory
 # from dotenv import load_dotenv
-from tools import AgentTool
+from tools import *
+from langchain.agents import AgentType, Tool, initialize_agent
 
 def _init_test_agent(session_id, streaming=False):
     llm_chat = ChatOpenAI(temperature=0.2, model="gpt-3.5-turbo-16k-0613", streaming=streaming)
     tools = [
-        get_tool("retriever")(
-            syllabus_vectorstore(),
-            name="syllabus_database",
-            description="retrieve manufacturing techology syllabus data"
-        ),
-        # get_tool(AgentTool.TELEGRAM)(
-        #     description="used to send a message to the teacher in case the user wanted a human to answer him."
-        # )
+        get_general_task(),
+        get_schedule_task(),
+        get_time_management_tool(),
     ]
-
+    # tool for taking task as (text, date, start time, end time)
+    # tool for the user entering the steps of the task (task_name, steps)
+    # tool for the user suffering from a problem
     sys_message = SystemMessage(
-        content="You are a manufacturing technology teacher. You help students in their questions about the syllabus.\n"
-                "You always lookup question in the syllabus_database with the same user question.\n"
-                "You NEVER answer questions outside the syllabus, and never come up with answers.\n"
-                "You always try to refer to the syllabus and in which lesson or topic the answer is if you can.\n"
-                "Query the syllabus_database with the exact user message without any modifications.\n"
-                "Be helpful and always willing to answer more and more questions.\n"
-                "Make your answers in bullet points whenever possible, don't make the answer too long, try to make it as short as possible.\n"
-                "Try to make your response short without losing information. (Answer Around 50 WORDS MAXIMUM)\n"
-                "Begin the conversation with offering help in manufacturing technology questions.\n"
-
+        content="You are an AI agent responsible for taking tasks from the user in a todo app, and answering questions related to time management and procrastination problems.\n"
+                "The user will interact with you in those ways:\n"
+                "1)If a user will send a task, you should ask the user for (start_time, end_time, date) of the task if the user didn't send it use schedule_task tool.\n"
+                "Make sure to always take from the user the (start_time, end_time, date) of the task. NEVER FORGET THEM!!\n\n"
+                "2)If a user will send you the steps of the task use the general_task tool to save the steps of the task and its title.\n\n"
+                "3)If a user will send you a question regarding time management and procrastination, answer only based on the data from the time_management_database.\n"
+                "Never answer anything from outside the time_management_database.\n\n"
+                "Make sure to extract all the details from the user message according to the tools you have.\n\n"
+                "If the user didn't give you full information, ask the user for the missing information.\n\n"
+                "Never answer any questions that is not related to the todo app, or time management and procrastination.\n\n"
+                "Never answer any general questions, REMEMBER that you are only a todo app agent.\n\n"
+                "Use the tools you have intelligently."
     )
 
     prompt = OpenAIFunctionsAgent.create_prompt(
@@ -41,10 +41,11 @@ def _init_test_agent(session_id, streaming=False):
         extra_prompt_messages=[MessagesPlaceholder(variable_name="chat_history")],
     )
 
-    reminder = "NEVER come up with answers. Always refer to the syllabus and in which lesson the answer is whenever possible.\n"
-    reminder += "Make your answers in bullet points whenever possible.\n"
-    reminder += "Don't make the answer too long, try to make it short without losing information.\n"
-    reminder += "Answer Around 50 WORDS MAXIMUM."
+    reminder = "Never Answer any general questions, REMEMBER that you are only a todo app agent, and only answer questions about time management and procrastination.\n\n"
+    reminder += "Be intelligent and use the tools you have intelligently.\n\n"
+    reminder += "use schedule_task tool to save the text, date, start time and end time of the task.\n\n"
+    reminder += "use general_task tool to save the steps of the task and its title."
+    reminder += "use time_management_database to answer questions about time management and procrastination."
 
     memory = AgentTokenBufferMemory(max_token_limit=10500, memory_key="chat_history", llm=llm_chat,
                                     chat_memory=DynamoDBChatMessageHistoryNew(table_name="langchain-agents",
@@ -57,6 +58,7 @@ def _init_test_agent(session_id, streaming=False):
         tools=tools,
         memory=memory,
         return_intermediate_steps=True,
+        verbose=True,
     )
 
     return agent_executor
--- tools.py
+++ tools.py
@@ -7,12 +7,114 @@ from pydantic import BaseModel, Field
 from langchain.vectorstores.base import VectorStoreRetriever, VectorStore
 from enum import Enum
 from dotenv import load_dotenv
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import LLMChain
+import json
 
 load_dotenv()
 
 class SingleInputToolSchema(BaseModel):
     query: str = Field(..., min_length=1)
 
+class GeneralTaskToolSchema(BaseModel):
+    task_name: str = Field(..., min_length=1, description="Name of the task according to the steps given by the user")
+    steps: str = Field(..., min_length=1, description="Steps of the task given by the user")
+
+
+class ScheduleTaskToolSchema(BaseModel):
+    text: str = Field(..., min_length=1, description="Text of the user message for the task")
+    date: str = Field(..., min_length=1, description="Date of the task")
+    start_time: str = Field(..., min_length=1, description="Start time of the task")
+    end_time: str = Field(..., min_length=1, description="End time of the task")
+
+class TimeManagementToolSchema(BaseModel):
+    query: str = Field(..., min_length=1, description="User question about time management and procrastination")
+
+def time_management_tool(query):
+
+    """
+    replace this with a vector database
+    """
+
+    # return some dummy information about time management and procrastination
+    text = "Time management is the process of planning and controlling how much time to spend on specific activities.\n"
+    text += "Good time management enables an individual to complete more in a shorter period of time, lowers stress, and leads to career success.\n"
+    text += "This guide provides a list of the top tips for managing time well.\n"
+    text += "This includes setting clear goals, breaking down goals into tasks, assigning time estimates to those tasks, and understanding the work environment.\n"
+    text += "This guide also provides a list of common time management errors.\n"
+    text += "These include focusing on the wrong tasks, failing to prioritize, and failing to delegate.\n"
+    text += "Finally, this guide provides a list of time management tools.\n"
+    text += "These include to-do lists, calendars, and project management software.\n"
+    text += "Time management is the process of planning and controlling how much time to spend on specific activities.\n"
+    text += "Good time management enables an individual to complete more in a shorter period of time, lowers stress, and leads to career success.\n"
+    text += "This guide provides a list of the top tips for managing time well.\n"
+    text += "This includes setting clear goals, breaking down goals into tasks, assigning time estimates to those tasks, and understanding the work environment.\n"
+    text += "This guide also provides a list of common time management errors.\n"
+    text += "These include focusing on the wrong tasks, failing to prioritize, and failing to delegate.\n"
+    text += "Finally, this guide provides a list of time management tools.\n"
+
+    return text
+
+def get_time_management_tool():
+    return StructuredTool(
+        name="time_management_database",
+        description="Use this tool to answer user questions about time management and procrastination.",
+        func=time_management_tool,
+        args_schema=TimeManagementToolSchema,
+    )
+def schedule_task_tool(text, date, start_time, end_time):
+    # llm_Chain for converting start time and end time to date time format
+    return f"Text: {text}\nDate: {date}\nStart time: {start_time}\nEnd time: {end_time}"
+def general_task_tool(task_name, steps):
+
+    model = ChatOpenAI(temperature=0.2, model="gpt-3.5-turbo-16k-0613", streaming=False)
+
+    _json = model.predict(
+        "You will take a task name and its steps in numerical order.\n"
+        "You should return for me a json object with the task name and its steps.\n"
+        "Example:\n"
+        "Task name: Do the laundry\n"
+        "Steps: 1) Put the clothes in the washing machine\n"
+        "2) Put the washing liquid\n"
+        "3) Turn on the washing machine\n\n"
+        "Json:\n"
+        "{\n"
+        "    'task_name': 'Do the laundry',\n"
+        "    'steps': [\n"
+        "        'Put the clothes in the washing machine',\n"
+        "        'Put the washing liquid',\n"
+        "        'Turn on the washing machine'\n"
+        "    ]\n"
+        "}\n\n"
+        "Here is the task name and its steps:\n"
+        f"Task name: {task_name}\n"
+        f"Steps: {steps}\n\n"
+        "Please return the json object with the task name and its steps.",
+    )
+
+    # trim json starting from the first { to the last }
+    # add this to a try catch block
+    _json = _json[_json.find("{"):_json.rfind("}")+1]
+
+    _json = json.loads(_json)
+
+    return f"Task name: {task_name}\nSteps: {steps}"
+
+def get_general_task():
+    return StructuredTool(
+        name="general_task",
+        description="Use this tool to save the steps of the task and its title, if the user entered the task in steps.",
+        func=general_task_tool,
+        args_schema=GeneralTaskToolSchema,
+    )
+
+def get_schedule_task():
+    return StructuredTool(
+        name="schedule_task",
+        description="Use this tool to save the text, date, start time and end time of the task.",
+        func=schedule_task_tool,
+        args_schema=ScheduleTaskToolSchema,
+    )
 
 class SendToMakeArgsSchema(BaseModel):
     name: str = Field(..., min_length=1)
